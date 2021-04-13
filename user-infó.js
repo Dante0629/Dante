f161f406aec08c004996763e93066155c707337b
Dante0629 <81125754+Dante0629@users.noreply.github.com>
2021-04-13 18:26:08 +0200

    Add files via upload

---
+++ user-infó.js
@@ -0,0 +1,43 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const moment = require('moment')
+moment.locale('HU')
+
+module.exports = (client) => {
+
+client.on('guildMemberAdd', async(member, message) => {
+    const Channel = member.guild.channels.cache.get('831457489186455562') 
+        const status = {
+            online: '🟢',
+            idle: '🟡',
+            dnd: '🔴 Ne zavarj',
+            offline: '⚫'
+        }
+    
+    const embed = new MessageEmbed()
+        .setColor('RED')
+        .setThumbnail('https://i2.wp.com/er23.hu/wp-content/uploads/2018/07/logo-big-information.png?fit=272%2C244&ssl=1') 
+        .setTitle(`**${member.user.username}-ról információk:**`)
+        .addFields(
+            {
+                name: `**Tag tag-je:**`,
+                value: `${member.user.tag}`
+            },
+            {
+                name: `**Csatlakozás dátuma:**`,
+                value: new Date(member.joinedTimestamp).toLocaleDateString()
+            },
+            {
+                name: `**Account létrehozásának a dátuma:**`,
+                value: `${moment.utc(member.user.createdAt).format('LLLL')}`
+            },
+            {
+                name: `**Állapota:**`,
+                value: `${status[member.presence.status]}`
+            }
+            )
+        .setTimestamp()
+        .setFooter(`Létrehozva:`)
+
+   Channel.send(embed)
+    })
+} 
